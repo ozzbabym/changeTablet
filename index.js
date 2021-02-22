d09c189f6b89a94ec24f2de299e72b013580c118
Ivan’ko <62439487+ozzbabym@users.noreply.github.com>
2021-02-22 19:20:25 +0300

    old file
    new code in files

--- index.js
+++ index.js
@@ -1,32 +1,35 @@
-var changeColor = document.getElementById('colorChange')
-var red = document.getElementById('red') 
-var green = document.getElementById('green') 
-var blue = document.getElementById('blue') 
-var num = document.getElementById('numChange')
-var button = document.getElementById('button')
-var numbers = document.querySelectorAll('tr>td')
+var changeColor = document.getElementById("colorChange");
+var red = document.getElementById("red");
+var green = document.getElementById("green");
+var blue = document.getElementById("blue");
+var num = document.getElementById("numChange");
+var button = document.getElementById("button");
+var numbers = document.querySelectorAll("tr>td");
+var table = document.querySelector('table')
 
-var selectColor
-changeColor.addEventListener('click',(e)=>{
-    if(!selectColor){
-        selectColor = 'white'
+table.addEventListener("click", (e) => {
+    num.value = e.target.innerHTML;
+});
+
+var selectColor;
+changeColor.addEventListener("click", (e) => {
+  if (!selectColor) {
+    selectColor = "white";
+  }
+  selectColor = e.target.innerHTML;
+  red.className = "";
+  green.className = "";
+  blue.className = "";
+  e.target.className = "changeColor";
+});
+
+var button = button.addEventListener("click", (e) => {
+  for (var i = 0; i < numbers.length; i++) {
+    if (i + 1 === parseInt(num.value)) {
+      numbers[i].style = `color: blue; font-style: italic;`;
+    } else {
+      numbers[i].style = `background: ${selectColor} ;`;
     }
-    selectColor = e.target.innerHTML
-    red.className =''
-    green.className =''
-    blue.className =''
-    e.target.className='changeColor'
-})
-
-var button = button.addEventListener('click', (e)=>{
-    for(var i = 0;i<numbers.length; i++){
-        if(i+1===parseInt(num.value)){
-            numbers[i].style=`color: blue; font-style: italic;`
-        }else{
-        numbers[i].style=`background: ${selectColor} ;`
-        } 
-    }
-    num.value =''
-
-})
-
+  }
+  num.value = "";
+});
